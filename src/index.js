b2e69dc9cae303e49b2cd5f6a946a3242055f65d
inaa123 <dlsdk7912@naver.com>
2023-12-11 23:06:12 +0900

    관리자 수정

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useAuthContext } from './context/AuthContext';
 
 //컴포넌트
 import App from './App';
@@ -10,7 +11,6 @@ import NotFound from './pages/NotFount';
 import MyCart from './pages/MyCart';
 import ProductDetail from './pages/ProductDetail';
 import UploadProduct from './pages/UploadProduct';
-import { useAuthContext } from './context/AuthContext';
 
 //css
 import './index.css';
@@ -18,15 +18,13 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //관리자 인증(조건에 하나라도 만족하지 못하면 페이지를 이동할 수 없게 하고, 강제로 홈으로 이동시킨다.(경고창 띄우지않음))
-const ProtectRouter = ({checkAdmin, children}) => {
-  const {user} = useAuthContext();
+const ProtectRouter = ({checkAdmin, children}) => {//checkAdmin과 children을 체크한다.
+  const { user } = useAuthContext(); //{user}유저에 대한 정보를 받아서 useAuthContex를 불러온다.
+  if(!user || (checkAdmin && !user.isAdmin)){ //user가 아니거나 (체크어드민과 user.isAdmin이 없으면 = 관리자가 아닌 경우) 강제로 홈으로 이동
+    return <Navigate to='/' replace/>
+  }
+  return children
 }
-// const ProtectRouter = ({checkAdmin, children}) => { //checkAdmin과 children을 체크한다.
-//   const {user} = useAuthContext(); //{user}유저에 대한 정보를 받아서 useAuthContex를 불러온다.
-//   if(!user || (checkAdmin && !user.isAdmin)){ //user가 아니거나 (체크어드민이고 user.isAdmin이 아닌경우 = )관리자가 아닌 경우 강제로 홈으로 이동
-//     return <Navigate to='/' replace />
-//   }
-// }
 
 const routes = createBrowserRouter([
   {
@@ -41,8 +39,12 @@ const routes = createBrowserRouter([
       //상세페이지 : 고유의 아이디 값을 받아, 여기선 아이템 등록할 때마다 아이디를 생성해야 함. -> 그 아이디를 detail페이지에 뿌려야함, id는 고정값X 변동되는 값
       {
         path : '/product/upload', 
-        element : 
-          <UploadProduct />
+        element :
+        <ProtectRouter checkAdmin>
+          <UploadProduct /> 
+        </ProtectRouter>
+
+          //UploadProduct에 들어갈 때 ProtectRouter를 실행(checkAdmin을 따지고 실행)
       }, // product/upload경로로 이동하게 되면 <UploadProduct>가 나오도록
     ]
   }
