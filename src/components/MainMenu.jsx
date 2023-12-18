647b216d4debcfc6ee8df47b362bd31e7ad39cd3
inaa123 <dlsdk7912@naver.com>
2023-12-18 17:08:01 +0900

    css수정

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react'
 import { CategoryContext } from '../context/CategoryContext'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 function MainMenu() {
     const {categoryList} = useContext(CategoryContext);
 
     return (
-        <nav>
+        <NavMenu>
             <ul>
                 {categoryList.map((el, index) => (
                     <li key={index}>
@@ -14,8 +15,26 @@ function MainMenu() {
                     </li>
                 ))}
             </ul>
-        </nav>
+        </NavMenu>
     )
 }
 
 export default MainMenu
+
+const NavMenu = styled.nav`
+    ul{
+        display: flex;
+        gap: 20px;
+        li{
+            a{
+                color: black;
+                &:hover{
+            
+                     color:#c17c74;
+         
+                }
+            }
+        }
+        
+    }
+`
